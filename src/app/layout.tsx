962052371dc3af33398fe2218bca4c41c5d3a8ac
arif <moarifur@gmail.com>
2025-08-17 13:38:47 +0600

    Fix all title, favicon and logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Trendlama - Best Clothes",
-  description: "Trendlama is the best place to find the best clothes",
+  title: "RectaShop",
+  description: "RectaShop is the best place to find the best clothes",
 };
 
 export default function RootLayout({
